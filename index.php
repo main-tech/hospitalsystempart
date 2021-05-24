dbd4d8b8c7cfe4a541171b8468410a0f069cdbf2
mandela <muithi.mandela@students.jkuat.ac.ke>
2021-05-25 01:19:48 +0300

    another commit

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 </head>
 <body>
 <form method="post" action="action.php">
-drug code:<input name="drug_code" type="text">
+drug code:<input id="drug_code" name="drug_code" type="text">
 <br>
-quantity:<input name="quantity" type="number">
+quantity:<input id="quantity" name="quantity" type="number">
 <br>
  <input type="submit"value="request to pharmacy">
 </form>
